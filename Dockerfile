87e8fb9c1a6763d0dfa7a441ac105ccb9b6cc065
Herrero Corralo, Luis <lherreroc@indra.es>
2021-11-23 21:57:15 +0100

    Fixed missing resource in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip setuptools wheel && \
     pip install --no-cache-dir -r requirements.txt
 COPY image_utils/ image_utils/
+COPY MANIFEST.in MANIFEST.in
 COPY setup.py setup.py
-RUN pip install --no-cache-dir .
+RUN pip install .
 ENTRYPOINT ["python", "-m", "image_utils.image_utils"]
--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='image-utils',
     version='0.1.0',
-    packages=find_packages(include=['image_utils', 'image_utils.*']),
+    packages=find_packages(include=['image_utils', 'image_utils.*', 'image_utils.resources.*']),
+    include_package_data=True,
+    package_data={'': ['image_utils/resources/*.tif']},
     install_requires=[
         'rasterio==1.1.0',
         'opencv-python-headless==4.5.4.58'
